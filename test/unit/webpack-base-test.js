0ce541a187c7d4d63dc61de2a434ace447b927ed
liangchaofei <chaofei_liang@126.com>
2020-03-24 23:02:55 +0800

    fix:a

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-24 17:53:30
- * @LastEditTime: 2020-03-24 22:55:52
+ * @LastEditTime: 2020-03-24 23:02:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /webpack_learn/10_demo/build-webpack/test/unit/webpack-unit-test.js
@@ -9,9 +9,10 @@
 
 const assert = require('assert');
 
-describe('webpack.base.js test case',() => {
+describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
-    it('entry',()=>{
-        assert.equal(baseConfig.entry.index.indexOf('build-webpack/test/smoke/template/src/index/index.js'>-1),true)
-    })
-})
+
+    it('entry', () => {
+        assert.equal(baseConfig.entry.index.indexOf('builde-webpack/test/smoke/template/src/index/index.js')>-1,true);
+    });
+});
